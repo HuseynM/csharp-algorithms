93df7735f341c6f388f930518655c9f39be4d262
Sanan Fataliyev <sanan.fataliyev@gmail.com>
2018-09-18 18:37:47 +0400

    package fix and test class added

---
+++ Algorithms.Tests/OperatorsAndExpressionsTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using static Algorithms.Exercises.operators_and_expressions.OperatorsAndExpressions;
+
+namespace Algorithms.Tests
+{
+    [TestFixture]
+    public class OperatorsAndExpressionsTests
+    {
+        [Test]
+        public void IsOddOrEvenTest()
+        {
+            Assert.AreEqual("even", IsOddOrEven(2));
+            Assert.AreEqual("even", IsOddOrEven(0));
+            Assert.AreEqual("even", IsOddOrEven(-6));
+            Assert.AreEqual("even", IsOddOrEven(int.MinValue));
+
+            Assert.AreEqual("odd", IsOddOrEven(1));
+            Assert.AreEqual("odd", IsOddOrEven(-1));
+            Assert.AreEqual("odd", IsOddOrEven(int.MaxValue));
+
+        }
+
+    }
+}
