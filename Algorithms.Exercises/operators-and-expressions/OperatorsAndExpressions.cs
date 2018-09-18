79c96f8c7894296fc3e1158adbcc4262a0101909
Huseyn <huseyn.mikayil95@gmail.com>
2018-09-18 06:05:57 -0700

    IsOddOrEven problem resolved

--- Algorithms.Exercises/operators-and-expressions/OperatorsAndExpressions.cs
+++ Algorithms.Exercises/operators-and-expressions/OperatorsAndExpressions.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Algorithms.Exercises.operators_and_expressions
+﻿namespace Algorithms.Exercises.operators_and_expressions
 {
     public class OperatorsAndExpressions
     {
+        public string IsOddOrEven(int value)
+        {
+            /*
+            * Write an expression that checks whether an integer is odd or even.
+            */
+
+            return (value & 1) == 0 ? "even" : "odd";
+        }
     }
 }
