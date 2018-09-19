e275252fd001141007eac974f49c50bd32f13ef5
Huseyn <huseyn.mikayil95@gmail.com>
2018-09-19 02:23:31 -0700

    CheckThirdBit resolved

--- Algorithms.Exercises/operators-and-expressions/OperatorsAndExpressions.cs
+++ Algorithms.Exercises/operators-and-expressions/OperatorsAndExpressions.cs
@@ -2,7 +2,7 @@
 {
     public static class OperatorsAndExpressions
     {
-        public static string IsOddOrEven(int value)
+        public static string IsOddOrEven(int value) // Exercise 1
         {
             /*
             * Write an expression that checks whether an integer is odd or even.
@@ -11,7 +11,7 @@
             return (value & 1) == 0 ? "even" : "odd";
         }
 
-        public static bool IsDivisibleBothDivisor(int value, int divisor1, int divisor2)
+        public static bool IsDivisibleBothDivisor(int value, int divisor1, int divisor2) // Exercise 2
         {
             /*
              * Write a Boolean expression that checks whether a given integer is
@@ -22,7 +22,7 @@
             return false;
         }
 
-        public static bool CheckThirdDigit(int value, int n)
+        public static bool CheckThirdDigit(int value, int n) // Exercise 3
         {
             /*
              * Write an expression that looks for a given integer if its third digit (right
@@ -32,6 +32,24 @@
             value = value / 100;
 
             return value % 10 == n;
+        }
+
+        public static bool CheckThirdBit(int value) // Exercise 4
+        {
+            /*
+             * Write an expression that checks whether the third bit in a given integer
+                is 1 or 0.
+             */
+
+            /*
+             * 1011010001
+             * 0000000100 4
+             * 0000000000
+             */
+
+            int thirdBit = value & 0B100; //0B100 = 4
+
+            return thirdBit == 0;
         }
     }
 }
