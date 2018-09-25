4f511e44cc71060435e621fa5d6667b5541cc919
Huseyn <huseyn.mikayil95@gmail.com>
2018-09-24 22:20:17 -0700

    ExchangeBitValues added

--- Algorithms.Exercises/operators-and-expressions/OperatorsAndExpressions.cs
+++ Algorithms.Exercises/operators-and-expressions/OperatorsAndExpressions.cs
@@ -242,5 +242,15 @@ namespace Algorithms.Exercises.operators_and_expressions
             return n;
         }
 
+        public static int ExchangeBitValues(int n)
+        {
+            /*
+              Write a program that exchanges bits {p, p+1, …, p+k-1} with bits {q,
+                    q+1, …, q+k-1} of a given 32-bit unsigned integer.
+             */
+
+            return 0;
+        }
+
     }
 }
