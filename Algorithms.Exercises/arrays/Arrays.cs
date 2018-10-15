61b10f354164e5b7d702e5c686dd695cae4c59e0
HuseynM <huseyn-mikayil@mail.ru>
2018-10-15 04:34:29 -0700

    PrintToN resolved

--- Algorithms.Exercises/arrays/Arrays.cs
+++ Algorithms.Exercises/arrays/Arrays.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace Algorithms.Exercises.arrays
 {
-    public class Arrays
+    public static class Arrays
     {
+        public static void PrintToN(int n)
+        {
+            /*
+                Write a program that prints on the console the numbers from 1 to N.
+                The number N should be read from the standard input.
+             */
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
     }
 }
