5474c91a11b0da103cdacb2ef1159c40feb1d2eb
HuseynM <huseyn-mikayil@mail.ru>
2018-10-15 04:45:22 -0700

    PrintNotDivisible resolved

--- Algorithms.Exercises/arrays/Arrays.cs
+++ Algorithms.Exercises/arrays/Arrays.cs
@@ -8,7 +8,7 @@ namespace Algorithms.Exercises.arrays
 {
     public static class Arrays
     {
-        public static void PrintToN(int n)
+        public static void PrintToN(int n) // Exercise 1
         {
             /*
                 Write a program that prints on the console the numbers from 1 to N.
@@ -18,6 +18,21 @@ namespace Algorithms.Exercises.arrays
             for (int i = 0; i < n; i++)
             {
                 Console.WriteLine(i);
+            }
+        }
+
+        public static void PrintNotDivisible(int n, int a, int b) // Exercise 1
+        {
+            /*
+                Write a program that prints on the console the numbers from 1 to N,
+                which are not divisible by a and b simultaneously. The number N
+                should be read from the standard input.
+             */
+
+            for (int i = 0; i < n; i++)
+            {
+                if (n % a != 0 && n % b != 0)
+                    Console.WriteLine(i);
             }
         }
     }
